35b3dd6851e270600009989504f4817952ca5dbc
mostafa123457780-lab <mostafa123457780@gmail.com>
2026-07-18 13:32:14 +0300

    Add language context to HomePage component
    Wrap HomePage content with LanguageProvider and HomeLanguageWrapper.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
-import HeroSection from '@/components/HeroSection'
-import ServicesSection from '@/components/ServicesSection'
-import StatsSection from '@/components/StatsSection'
-import CTASection from '@/components/CTASection'
+import HeroSection from "@/components/HeroSection";
+import ServicesSection from "@/components/ServicesSection";
+import StatsSection from "@/components/StatsSection";
+import CTASection from "@/components/CTASection";
+import { LanguageProvider } from "@/lib/i18n/LanguageContext";
+import { HomeLanguageWrapper } from "@/components/HomeLanguageWrapper";
 
 export default function HomePage() {
   return (
-    <main>
-      <HeroSection />
-      <StatsSection />
-      <ServicesSection />
-      <CTASection />
-    </main>
-  )
+    <LanguageProvider>
+      <HomeLanguageWrapper>
+        <main>
+          <HeroSection />
+          <StatsSection />
+          <ServicesSection />
+          <CTASection />
+        </main>
+      </HomeLanguageWrapper>
+    </LanguageProvider>
+  );
 }
